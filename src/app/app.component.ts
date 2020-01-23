636854344a5ac17aaf27a7d774abc1f7c424d176
Nitai Charan <nitaicharan@gmail.com>
2020-01-23 15:47:36 -0300

    24. Usando o Decorator @Input
    - @Input()
    - []

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'course';
+  luke = { name: 'Luke', isJedi: true, temple: 'Coruscant' }
+  leia = { name: 'Leia', isJedi: false }
+  han = { name: 'Han Solo', isJedi: false }
 }
